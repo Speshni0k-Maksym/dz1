663a3fa16ab421a211251313749958f37e506562
Speshni0k-Maksym <popovichspgmaster1@gmail.com>
2024-06-27 19:08:29 +0300

    https://github.com/Speshni0k-Maksym/dz.git

--- dz3.py
+++
@@ -1,7 +0,0 @@
-text = input("Введіть 2 слова: ")
-text1 = text.split()
-if len(text1) >= 2:
-    print(text[::-1])
-    print(' '.join(text1[::-1]))
-else:
-    print("Рядок не містить мінімум 2 слова. Спробуйте ще раз.")
